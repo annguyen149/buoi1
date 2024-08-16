c7205258ddeb774529e273e152ca59a31a8b86d9
Nguyen Duc An <118339992+annguyen149@users.noreply.github.com>
2024-08-16 14:36:10 +0700

    Add files via upload

---
+++ buoi1_php/Part1_1/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        img{
+            width: 500px;
+            height: 500px;
+            object-fit: cover;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+echo "Hello, this is PHP script";
+echo '<h1 style="color: blue; text-align: center;">Xin chào TMU</h1>';
+echo'<img src="image.JPG" alt="">'
+
+?>
+ 
+</body>
+</html>
---
+++ buoi1_php/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        img {
+            width: 500px;
+            height: 500px;
+            object-fit: cover;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        echo"Hello, TMU";
+        echo'<h1 style="color:blue; text-align:center;">Xin chào</h1>';
+
+    ?>
+    <img src="https://vcdn1-dulich.vnecdn.net/2021/07/16/3-1-1626444927.jpg?w=460&h=0&q=100&dpr=2&fit=crop&s=KU8IkmrM5HbtYIyyS5k1qQ" alt="">
+</body>
+</html>
